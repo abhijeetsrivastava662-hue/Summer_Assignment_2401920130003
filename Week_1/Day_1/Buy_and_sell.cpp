7c7f7e893296f2962a51d73abb4a4e2371ff6135
abhijeetsrivastava662-hue <abhijeetsrivastava662@gmail.com>
2026-06-05 12:27:56 +0530

    Update Buy_and_sell.cpp

--- Week_1/Day_1/Buy_and_sell.cpp
+++ Week_1/Day_1/Buy_and_sell.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
-    int removeDuplicates(vector<int>& nums) {
-        int i=0;
-        for(int j=1;j<nums.size();j++){
-            if(nums[i]!=nums[j]){
-                nums[i+1]=nums[j];
-                i++;
-            }
-
+    int maxProfit(vector<int>& prices) {
+        int mini=prices[0];
+        int profit=0;
+        int n=prices.size();
+        for(int i=0;i<n;i++){
+            int cost = prices[i]-mini;
+            profit=max(profit,cost);
+            mini = min(mini,prices[i]);
         }
-        return i+1;
+    return profit;
         
     }
 };
